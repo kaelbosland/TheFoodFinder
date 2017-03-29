46910277c9eb1c09b791d941b151de1fb521c65f
Shanjutha <jeyarats@mcmaster.ca>
2017-03-29 00:29:45 -0400

    Update Sort.java

--- src/Sort.java
+++ src/Sort.java
@@ -1,7 +1,14 @@
-
 public class Sort{
-	
+	private int input[];
+    private int length;
 	//QuickSort 
+
+    public static void sort(Comparable[] a) {
+        StdRandom.shuffle(a);
+        sort(a, 0, a.length - 1);
+       
+    }
+
 	
 	private static void sort(Comparable[] prodID, int lo, int hi){
 		if (hi <= lo) 
@@ -34,5 +41,39 @@ public class Sort{
 	        prodID[i] = prodID[j];
 	        prodID[j] = swap;
 	    }
+	  
+	  private static void show(Comparable[] a) {
+	        for (int i = 0; i < a.length; i++) {
+	            StdOut.println(a[i]);
+	        }
+	    }
+	  
+	  public static Comparable select(Comparable[] a, int k) {
+	        if (k < 0 || k >= a.length) {
+	            throw new IndexOutOfBoundsException("Selected element out of bounds");
+	        }
+	        StdRandom.shuffle(a);
+	        int lo = 0, hi = a.length - 1;
+	        while (hi > lo) {
+	            int i = partition(a, lo, hi);
+	            if      (i > k) hi = i - 1;
+	            else if (i < k) lo = i + 1;
+	            else return a[i];
+	        }
+	        return a[lo];
+	    }
+
+	
+	    //test case 
+	  public static void main(String[] args) {
+	        Integer[] a = {4,8,6,5,2,8};
+	        Sort.sort(a);
+	        StdOut.println();
+	        for (int i = 0; i < a.length; i++) {
+	            Integer ith = (Integer) Sort.select(a, i);
+	            StdOut.println(ith);
+	    }
+	  }
+	  
 }
 
